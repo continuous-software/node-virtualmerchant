36bbfe2ce25e9f83577acd256c44b1dfe6ab60d8
Amine Mouafik <amine@mouafik.fr>
2015-02-16 19:30:15 +0700

    Adding tool to see test gateway responses coverage for each card network.

---
+++ util/coverage.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var testGatewayResponses = require('./TestGatewayResponses.js');
+
+Object.keys(testGatewayResponses).forEach(function (network) {
+  console.log(network + ':');
+  var errorCodes = [];
+  Object.keys(testGatewayResponses[network]).forEach(function (e) {
+    errorCodes = errorCodes.concat(testGatewayResponses[network][e]);
+  });
+  errorCodes.sort();
+  var notCovered = 0;
+  for (var i = 0; i < 99; i++) {
+    var current = (i < 10) ? '0' + i : i.toString();
+    if (current != errorCodes[i - notCovered]) {
+      console.log('Response #' + current + ' not covered by unit tests.');
+      notCovered++;
+    }
+  }
+});
